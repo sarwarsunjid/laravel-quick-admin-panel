98d83fffdc399e8dc4068b7d951212bc0e29bd37
Sarwar Jahan Bhuiyan <sarwarsunjid@gmail.com>
2021-10-09 16:59:35 +0600

    initial commit

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt(12345678),
+        ]);
+    }
+}
